165affe819338b093a33de951852d4c6bb0d448b
Yann Yu <yy250138@ncr.com>
2018-09-28 12:19:26 -0400

    clean up

--- ez_recover_emails_from_archive.py
+++ ez_recover_emails_from_archive.py
@@ -4,12 +4,12 @@ from win32com.client import constants
 outlook=win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
 # inbox = outlook.GetDefaultFolder(6)
-inbox = outlook.GetDefaultFolder(win32com.client.constants.olFolderInbox)
+# inbox = outlook.GetDefaultFolder(win32com.client.constants.olFolderInbox)
 
-messages=inbox.Items
+# messages=inbox.Items
 
 root_folder = outlook.Folders.Item(1)
-# to get to Cloud_Automation folder
+# to get to folder
 folder_andrew = root_folder.Folders['Andrew']
 
 for message in messages:
