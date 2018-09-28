f2e0f9f192a97bf3cf066cf8e2248270d5430abc
Yann Yu <yy250138@ncr.com>
2018-09-28 12:15:53 -0400

    add move emails

---
+++ ez_move_emails.py
@@ -0,0 +1,20 @@
+import win32com.client
+from win32com.client import constants
+
+outlook=win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+# inbox = outlook.GetDefaultFolder(6)
+inbox = outlook.GetDefaultFolder(win32com.client.constants.olFolderInbox)
+
+messages=inbox.Items
+
+root_folder = outlook.Folders.Item(1)
+# to get to Cloud_Automation folder
+folder_andrew = root_folder.Folders['Andrew']
+
+for message in messages:
+    # print(message.Sender.Address)
+    if "Reusche" in message.Sender.Name:
+        print("-"*70)
+        print(message.Subject)
+        message.Move(folder_andrew)
